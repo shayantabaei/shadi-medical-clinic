3c4bdb42ee272aa81f1e4d17f3032d7c49e45468
shayantabaei <shayantabaei@gmail.com>
2018-01-23 21:17:49 -0500

    base skeleton

--- tabaei-medical/src/app/app.module.ts
+++ tabaei-medical/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuDropdownComponent } from './menu-dropdown/menu-dropdown.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuDropdownComponent
   ],
   imports: [
     BrowserModule
