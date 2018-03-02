c8b82df1194a6757bebd393e25d9f58666a91846
shayantabaei <shayantabaei@gmail.com>
2018-03-02 17:30:57 -0500

    Deployed Code - March 2nd 2018

--- tabaei-medical/src/app/header/header.component.ts
+++ tabaei-medical/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+    
+    public goToSection(section) {
+        if(section == 'conditions') {
+            $('html, body').animate({
+                scrollTop: $("#conditionsTreated").offset().top
+            }, 300);
+        }
+    }
 
   ngOnInit() {
+      
   }
 
 }
